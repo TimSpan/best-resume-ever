34e9a25be6e163090121a4adc5972934c624a385
TimSpan <cat20201314@163.com>
2024-01-30 16:31:41 +0800

    first commit

---
+++ src/lang/en.js
@@ -0,0 +1,14 @@
+// English
+const lang = {
+  contact: '联系方式',
+  born: 'Born',
+  bornIn: 'in',
+  experience: '工作经验',
+  education: '毕业院校',
+  skills: '专业技能',
+  projects: '项目简介',
+  contributions: '自我评价',
+  about: '自我评价',
+
+};
+export default lang;
